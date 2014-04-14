a94aab4b6762bbb996a2ec040d0cca5bf05fe9ab
Giom <gdromer@oodrive.fr>
2014-04-14 09:51:27 +0200

    bootstrap

--- src/main/java/controller/Alarme.java
+++ src/main/java/controller/Alarme.java
@@ -5,6 +5,18 @@ package controller;
  */
 public class Alarme {
 
+    private Piece piece ;
 
+    public Alarme(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
 
 }
--- src/main/java/controller/Refrigerateur.java
+++ src/main/java/controller/Refrigerateur.java
@@ -1,7 +1,33 @@
 package controller;
 
+import enumeration.AlimentationTypeENUM;
+
 /**
  * Created by g.dromer on 09/04/14.
  */
 public class Refrigerateur {
+
+    private AlimentationTypeENUM alimentationType ;
+    private Piece nomPiece ;
+
+    public Refrigerateur(AlimentationTypeENUM alimentationType, Piece nomPiece) {
+        this.alimentationType = alimentationType;
+        this.nomPiece = nomPiece;
+    }
+
+    public AlimentationTypeENUM getAlimentationType() {
+        return alimentationType;
+    }
+
+    public void setAlimentationType(AlimentationTypeENUM alimentationType) {
+        this.alimentationType = alimentationType;
+    }
+
+    public Piece getNomPiece() {
+        return nomPiece;
+    }
+
+    public void setNomPiece(Piece nomPiece) {
+        this.nomPiece = nomPiece;
+    }
 }
